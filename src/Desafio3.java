b814dc3f7645684c75aa414da09ca0126fb5a98b
gabrielrsh <gabriel.shiota@gmail.com>
2023-08-24 02:59:07 -0300

    fix: melhorado Desafio3

--- src/Desafio3.java
+++ src/Desafio3.java
@@ -5,11 +5,7 @@ public class Desafio3 {
     private List<Integer> numeros = Arrays.asList(-1, -2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
 
     public void verificarNumerosPositivos() {
-        long quantidadeNegativos = numeros.stream().filter(n->n < 0).count();
-        if( quantidadeNegativos > 0 )
-            System.out.println("Existem " + quantidadeNegativos + " numeros negativos na lista");
-        else 
-            System.out.println("Todos os números da lista são positivos");
+        System.out.println("Existem " + numeros.stream().filter(n->n < 0).count() + " numeros negativos na lista");
     }
 
     public static void main(String[] args) {
