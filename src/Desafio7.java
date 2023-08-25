2c1adfeb7c3c9be00387b5dae0c381ff57a36032
gabrielrsh <gabriel.shiota@gmail.com>
2023-08-24 22:21:03 -0300

    fix: Desafio7 modificado para usar apenas stream

--- src/Desafio7.java
+++ src/Desafio7.java
@@ -1,16 +1,18 @@
 import java.util.Arrays;
 import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
 
 public class Desafio7 {
     private List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
 
     public void segundoMaiorNumero() {
         //colocar em um set para retirar valores duplicados
-        Set<Integer> conjunto = new TreeSet<>(numeros.stream().sorted().toList());
+        //Set<Integer> conjunto = new TreeSet<>(numeros.stream().sorted().toList());
         //converter para um array e acessar a penultima posicao
-        System.out.println(conjunto.toArray()[conjunto.size()-2]);
+        //System.out.println(conjunto.toArray()[conjunto.size()-2]);
+
+        int tamanhoListaSemRepeticao = (int) numeros.stream().distinct().count();
+        Object segundoMaior = numeros.stream().distinct().toArray()[tamanhoListaSemRepeticao-2];
+        System.out.println(segundoMaior);
     }
 
     public static void main(String[] args) {
